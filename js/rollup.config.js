f423cb32776eef8d84a106ece44f27046465112d
Nicolas Martinussen <me@whaxion.be>
2020-10-22 19:12:37 +0200

    feat: Initial commit

---
+++ js/rollup.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    input: 'src/main.js',
+    output: {
+        file: '../public/js/bundle.js',
+        format: 'iife',
+        compact: true
+    }
+};
---
+++ js/src/main.js
@@ -0,0 +1,41 @@
+import {randomNumber} from "./utils";
+
+let texts = [
+    "étudiant en informatique",
+    "développeur web"
+];
+let currentText = "";
+let currentPos = 0;
+
+function main() {
+    setTimeout(eraseText, randomNumber(2000, 3500));
+}
+
+function writeText(){
+    let iAmA = document.getElementById("i-am-a");
+    if(!currentText || currentText.length <= 0){
+        if(++currentPos >= texts.length){
+            currentPos = 0;
+        }
+        currentText = texts[currentPos];
+    }
+    iAmA.innerText += currentText.charAt(0);
+    currentText = currentText.slice(1);
+    if(currentText.length <= 0){
+        setTimeout(eraseText, randomNumber(2000, 3500));
+    } else {
+        setTimeout(writeText, randomNumber(50, 100));
+    }
+}
+
+function eraseText(){
+    let iAmA = document.getElementById("i-am-a");
+    if(iAmA.innerText.length > 0){
+        iAmA.innerText = iAmA.innerText.slice(0, -1);
+        setTimeout(eraseText, randomNumber(50, 100));
+    } else {
+        setTimeout(writeText, randomNumber(1000, 1500));
+    }
+}
+
+window.onload = main;
